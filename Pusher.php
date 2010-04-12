aa3aa2d3f6adf4811183683f1782b39b1be4a279
Squeeks <privacymyass@gmail.com>
2010-04-12 13:50:54 +0100

    Initial commit.

---
+++ Pusher.php
@@ -0,0 +1,73 @@
+<?php
+
+/* 
+		Pusher PHP Library
+	/////////////////////////////////
+	This is a very simple PHP library to the Pusher API.
+
+		$pusher = new Pusher(APIKEY, CHANNEL, [Debug: true/false, HOST, PORT]);
+		$pusher->trigger('my_event', 'test_channel', [Debug: true/false]);
+
+	Copyright 2010, Squeeks. Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php 
+	
+*/
+
+class Pusher
+{
+	private $settings = array();
+
+	public function __construct($api_key, $channel, $debug = false, $host = 'http://api.pusherapp.com', $port = '80')
+	{
+		
+		// Check for dependent PHP extensions (SimpleXML, curl, pcre, xml, zlib)
+		if(!extension_loaded('curl') || !extension_loaded('json'))
+		{
+			die("There is missing dependant extensions - please ensure both cURL and JSON modules are installed");
+		}
+
+		$this->settings['server']  = $host;
+		$this->settings['port']    = $port;
+		$this->settings['api_key'] = $api_key;
+		$this->settings['channel'] = $channel;
+		$this->settings['url']     = $this->settings['server'].'/app/'.$this->settings['api_key'].'/channel/'.$this->settings['channel'];
+
+	}
+
+	public function trigger($event, $data, $debug = false)
+	{
+		if($ch = curl_init())
+		{
+			$payload = array(event => $event, data => $data);
+
+			curl_setopt($ch, CURLOPT_URL, $this->settings['url']);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+
+			$response = curl_exec($ch);
+			curl_close($ch);
+
+			if($response = 'OK')
+			{ 
+			 	return true;
+			}
+			elseif($debug == true || $this->settings['debug'] == true)
+			{
+				return $response;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+		else
+		{
+			die("Could not initialise cURL!");
+		}
+		
+	}
+
+}
+
+?>
---
+++ README.md
@@ -0,0 +1,27 @@
+Pusher PHP Library
+==================
+
+This is a very simple PHP library to the Pusher API (http://pusherapp.com).
+Using it is easy as pie:
+
+    require('Pusher.php');
+
+    $pusher = new Pusher($your_pusher_api_key, $channel);
+    $pusher->trigger('my_event', 'test_channel');
+
+Debugging
+---------
+You can either turn on debugging by setting the third argument like so, to true:
+
+    $pusher->trigger('event', 'data', true)
+
+or with all requests:
+
+    $pusher = new Pusher($key, $channel, true);
+
+On failed requests, this will return the server's response, instead of false.
+
+License
+-------
+Copyright 2010, Squeeks. Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php 
+
