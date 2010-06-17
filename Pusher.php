8afa6e9a809cb8c2819890c2bb3934d9a9adead0
Squeeks <squeek@cpan.org>
2010-06-17 14:37:33 +0100

    This is not a big as commit as you think. It is just a whitespace clean up.

--- Pusher.php
+++ Pusher.php
@@ -18,141 +18,146 @@
 
 */
 
-class Pusher {
+class Pusher
+{
 
-  private $settings = array ();
+	private $settings = array ();
 
-  /**
-   * PHP5 Constructor. 
-   * 
-   * Initializes a new Pusher instance with key, secret , app ID and channel. 
-   * You can optionally turn on debugging for all requests by setting debug to true.
-   * 
-   * @param string $auth_key
-   * @param string $secret
-   * @param int $app_id
-   * @param string $channel [optional]
-   * @param bool $debug [optional]
-   * @param string $host [optional]
-   * @param int $port [optional]
-   * @param int $timeout [optional]
-   */
-  public function __construct( $auth_key, $secret, $app_id, $channel = '', $debug = false, $host = 'http://api.pusherapp.com', $port = '80', $timeout = 30 ) {
+	/**
+	* PHP5 Constructor. 
+	* 
+	* Initializes a new Pusher instance with key, secret , app ID and channel. 
+	* You can optionally turn on debugging for all requests by setting debug to true.
+	* 
+	* @param string $auth_key
+	* @param string $secret
+	* @param int $app_id
+	* @param string $channel [optional]
+	* @param bool $debug [optional]
+	* @param string $host [optional]
+	* @param int $port [optional]
+	* @param int $timeout [optional]
+	*/
+	public function __construct( $auth_key, $secret, $app_id, $channel = '', $debug = false, $host = 'http://api.pusherapp.com', $port = '80', $timeout = 30 )
+	{
 
-    // Check compatibility, disable for speed improvement
-    $this->check_compatibility();
-    
-    // Setup defaults
-    $this->settings['server']	= $host;
-    $this->settings['port']	= $port;
-    $this->settings['auth_key']	= $auth_key;
-    $this->settings['secret']	= $secret;
-    $this->settings['app_id']	= $app_id;
-    $this->settings['channel']	= $channel;
-    $this->settings['url']	= '/apps/' . $this->settings['app_id'];
-    $this->settings['debug']	= $debug;
-    $this->settings['timeout']	= $timeout;
-  
-  }
+		// Check compatibility, disable for speed improvement
+		$this->check_compatibility();
 
-  /**
-   * Check if the current PHP setup is sufficient to run this class
-   */
-  private function check_compatibility() {
+		// Setup defaults
+		$this->settings['server']	= $host;
+		$this->settings['port']	= $port;
+		$this->settings['auth_key']	= $auth_key;
+		$this->settings['secret']	= $secret;
+		$this->settings['app_id']	= $app_id;
+		$this->settings['channel']	= $channel;
+		$this->settings['url']	= '/apps/' . $this->settings['app_id'];
+		$this->settings['debug']	= $debug;
+		$this->settings['timeout']	= $timeout;
 
-    // Check for dependent PHP extensions (JSON, cURL)
-    if ( ! extension_loaded( 'curl' ) || ! extension_loaded( 'json' ) )
-    {
-	die( 'There is missing dependant extensions - please ensure both cURL and JSON modules are installed' );
-    }
+	}
 
-    # Supports SHA256?
-    if ( ! in_array( 'sha256', hash_algos() ) )
-    {
-	die( 'SHA256 appears to be unsupported - make sure you have support for it, or upgrade your version of PHP.' );
-    }
-  
-  }
+	/**
+	* Check if the current PHP setup is sufficient to run this class
+	*/
+	private function check_compatibility()
+	{
 
-  /**
-   * Trigger an event by providing event name and payload. 
-   * Optionally provide a socket ID to exclude a client (most likely the sender).
-   * 
-   * @param string $event
-   * @param mixed $payload
-   * @param int $socket_id [optional]
-   * @param string $channel [optional]
-   * @param bool $debug [optional]
-   * @return bool|string
-   */
-  public function trigger( $event, $payload, $socket_id = null, $channel = '', $debug = false ) {
+		// Check for dependent PHP extensions (JSON, cURL)
+		if ( ! extension_loaded( 'curl' ) || ! extension_loaded( 'json' ) )
+		{
+		die( 'There is missing dependant extensions - please ensure both cURL and JSON modules are installed' );
+		}
 
-    # Check if we can initialize a cURL connection
-    $ch = curl_init();
-    if ( $ch === false )
-    {
-	die( 'Could not initialise cURL!' );
-    }
+		# Supports SHA256?
+		if ( ! in_array( 'sha256', hash_algos() ) )
+		{
+		die( 'SHA256 appears to be unsupported - make sure you have support for it, or upgrade your version of PHP.' );
+		}
 
-    # Add channel to URL..
-    $s_url = $this->settings['url'] . '/channels/' . ($channel != '' ? $channel : $this->settings['channel']) . '/events';
-    
-    # Build the request
-    $signature = "POST\n" . $s_url . "\n";
-    $payload_encoded = json_encode( $payload );
-    $query = "auth_key=" . $this->settings['auth_key'] . "&auth_timestamp=" . time() . "&auth_version=1.0&body_md5=" . md5( $payload_encoded ) . "&name=" . $event;
-    
-    # Socket ID set?
-    if ( $socket_id !== null )
-    {
-	$query .= "&socket_id=" . $socket_id;
-    }
+	}
 
-    # Create the signed signature...
-    $auth_signature = hash_hmac( 'sha256', $signature . $query, $this->settings['secret'], false );
-    $signed_query = $query . "&auth_signature=" . $auth_signature;
-    $full_url = $this->settings['server'] . ':' . $this->settings['port'] . $s_url . '?' . $signed_query;
-    
-    # Set cURL opts and execute request
-    curl_setopt( $ch, CURLOPT_URL, $full_url );
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, array ( "Content-Type: application/json" ) );
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-    curl_setopt( $ch, CURLOPT_POST, 1 );
-    curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload_encoded );
-    curl_setopt( $ch, CURLOPT_TIMEOUT, $this->settings['timeout'] );
+	/**
+	* Trigger an event by providing event name and payload. 
+	* Optionally provide a socket ID to exclude a client (most likely the sender).
+	* 
+	* @param string $event
+	* @param mixed $payload
+	* @param int $socket_id [optional]
+	* @param string $channel [optional]
+	* @param bool $debug [optional]
+	* @return bool|string
+	*/
+	public function trigger( $event, $payload, $socket_id = null, $channel = '', $debug = false )
+	{
 
-    $response = curl_exec( $ch );
+		# Check if we can initialize a cURL connection
+		$ch = curl_init();
+		if ( $ch === false )
+		{
+			die( 'Could not initialise cURL!' );
+		}
 
-    curl_close( $ch );
-    
-    if ( $response == "202 ACCEPTED\n" && $debug == false )
-    {
-      return true;
-    }
-    elseif ( $debug == true || $this->settings['debug'] == true )
-    {
-      return $response;
-    }
-    else
-    {
-      return false;
-    }
-  
-  }
+		# Add channel to URL..
+		$s_url = $this->settings['url'] . '/channels/' . ($channel != '' ? $channel : $this->settings['channel']) . '/events';
 
-  /**
-   * Creates a socket signature
-   * 
-   * @param int $socket_id
-   * @return string
-   */
-  public function socket_auth( $socket_id ) {
+		# Build the request
+		$signature = "POST\n" . $s_url . "\n";
+		$payload_encoded = json_encode( $payload );
+		$query = "auth_key=" . $this->settings['auth_key'] . "&auth_timestamp=" . time() . "&auth_version=1.0&body_md5=" . md5( $payload_encoded ) . "&name=" . $event;
 
-    $signature = hash_hmac( 'sha256', $socket_id . ':' . $this->settings['channel'], $this->settings['secret'], false );
-    $signature = array ( 'auth' => $this->settings['auth_key'] . ':' . $signature );
-    return json_encode( $signature );
-    
-  }
+		# Socket ID set?
+		if ( $socket_id !== null )
+		{
+			$query .= "&socket_id=" . $socket_id;
+		}
+
+		# Create the signed signature...
+		$auth_signature = hash_hmac( 'sha256', $signature . $query, $this->settings['secret'], false );
+		$signed_query = $query . "&auth_signature=" . $auth_signature;
+		$full_url = $this->settings['server'] . ':' . $this->settings['port'] . $s_url . '?' . $signed_query;
+
+		# Set cURL opts and execute request
+		curl_setopt( $ch, CURLOPT_URL, $full_url );
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, array ( "Content-Type: application/json" ) );
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+		curl_setopt( $ch, CURLOPT_POST, 1 );
+		curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload_encoded );
+		curl_setopt( $ch, CURLOPT_TIMEOUT, $this->settings['timeout'] );
+
+		$response = curl_exec( $ch );
+
+		curl_close( $ch );
+
+		if ( $response == "202 ACCEPTED\n" && $debug == false )
+		{
+			return true;
+		}
+		elseif ( $debug == true || $this->settings['debug'] == true )
+		{
+			return $response;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
+	/**
+	* Creates a socket signature
+	* 
+	* @param int $socket_id
+	* @return string
+	*/
+	public function socket_auth( $socket_id )
+	{
+
+		$signature = hash_hmac( 'sha256', $socket_id . ':' . $this->settings['channel'], $this->settings['secret'], false );
+		$signature = array ( 'auth' => $this->settings['auth_key'] . ':' . $signature );
+		return json_encode( $signature );
+
+	}
 
 
 }
