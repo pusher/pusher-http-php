f698587ff14343025445bd6b76dd090ae25a9380
zimbatm <zimbatm@zimbatm.com>
2015-03-19 16:42:26 +0000

    ws
    Use tabs for indent

--- lib/PusherHTTPException.php
+++ lib/PusherHTTPException.php
@@ -1,11 +1,11 @@
 <?php
 
 class PusherHTTPException extends PusherException {
-  public $status;
-  public $body;
+	public $status;
+	public $body;
 
-  public function __construct($response) {
-    $this->status = $response['status'];
-    $this->body = $response['body'];
-  }
+	public function __construct($response) {
+		$this->status = $response['status'];
+		$this->body = $response['body'];
+	}
 }
--- lib/PusherTriggerResult.php
+++ lib/PusherTriggerResult.php
@@ -5,20 +5,20 @@
  */
 class PusherTriggerResult {
 
-  /**
-  * A lookup of the event ID of a trigger on a given channel.
-  * e.g. $ch1EventId = $triggerResult['ch1'];
-  * 
-  * @return array
-  */
-  public $eventIds = array();
-  
-  /**
-   * @param array $decodedJson The decoded JSON triggerresponse from the Pusher HTTP API.
-   */
-  public function __construct($decodedJson) {
-    foreach($decodedJson['event_ids'] as $channel => $eventId ) {
-      $this->eventIds[ $channel ] = $eventId;
-    }
-  }
+	/**
+	* A lookup of the event ID of a trigger on a given channel.
+	* e.g. $ch1EventId = $triggerResult['ch1'];
+	*
+	* @return array
+	*/
+	public $eventIds = array();
+
+	/**
+	 * @param array $decodedJson The decoded JSON triggerresponse from the Pusher HTTP API.
+	 */
+	public function __construct($decodedJson) {
+		foreach($decodedJson['event_ids'] as $channel => $eventId ) {
+			$this->eventIds[ $channel ] = $eventId;
+		}
+	}
 }
