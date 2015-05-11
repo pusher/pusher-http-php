33c33b2cfb8ef9cf12f11609fb479572c0ecad66
Will Sewell <me@willsewell.com>
2015-05-11 17:55:32 +0100

    Add more tests that check conditions with invalid : characters

--- test/unit/socketAuthTest.php
+++ test/unit/socketAuthTest.php
@@ -21,12 +21,76 @@
 				'Socket auth key valid');
 		}
 
+		public function testComplexSocketAuthKey()
+		{
+			$socket_auth = $this->pusher->socket_auth('-azAZ9_=@,.;', '45055.28877557');
+			$this->assertEquals($socket_auth,
+				'{"auth":"thisisaauthkey:d1c20ad7684c172271f92c108e11b45aef07499b005796ae1ec5beb924f361c4"}',
+				'Socket auth key valid');
+		}
+
 		/**
 		 * @expectedException PusherException
 		 */
-		public function testInvalidSocketThrowsException()
+		public function testTrailingColonSocketIDThrowsException()
 		{
-			$this->pusher->socket_auth('testing_pusher-php', 'invalid');
+			$this->pusher->socket_auth('testing_pusher-php', '1.1:');
+		}
+
+		/**
+		 * @expectedException PusherException
+		 */
+		public function testLeadingColonSocketIDThrowsException()
+		{
+			$this->pusher->socket_auth('testing_pusher-php', ':1.1');
+		}
+
+		/**
+		 * @expectedException PusherException
+		 */
+		public function testLeadingColonNLSocketIDThrowsException()
+		{
+			$this->pusher->socket_auth('testing_pusher-php', ':\n1.1');
+		}
+
+		/**
+		 * @expectedException PusherException
+		 */
+		public function testTrailingColonNLSocketIDThrowsException()
+		{
+			$this->pusher->socket_auth('testing_pusher-php', '1.1\n:');
+		}
+
+		/**
+		 * @expectedException PusherException
+		 */
+		public function testTrailingColonChannelThrowsException()
+		{
+			$this->pusher->socket_auth('test_channel:', '1.1');
+		}
+
+		/**
+		 * @expectedException PusherException
+		 */
+		public function testLeadingColonChannelThrowsException()
+		{
+			$this->pusher->socket_auth(':test_channel', '1.1');
+		}
+
+		/**
+		 * @expectedException PusherException
+		 */
+		public function testLeadingColonNLChannelThrowsException()
+		{
+			$this->pusher->socket_auth(':\ntest_channel', '1.1');
+		}
+
+		/**
+		 * @expectedException PusherException
+		 */
+		public function testTrailingColonNLChannelThrowsException()
+		{
+			$this->pusher->socket_auth('test_channel\n:', '1.1');
 		}
 	}
 
