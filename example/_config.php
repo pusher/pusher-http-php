df1c11b0def2691feaf4c183481b6dc52b5da533
zimbatm <zimbatm@zimbatm.com>
2015-07-03 15:05:42 +0100

    Fix example import

--- example/_config.php
+++ example/_config.php
@@ -7,4 +7,4 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 header('Internal Server Error', true, 500);
 require "../vendor/autoload.php";
 
-$pusher = new pusher\Pusher("https://9cd6b8b5de12be68eb47:7c1c1189729651b135f1@api.pusherapp.com/apps/79075");
+$pusher = new Pusher\Pusher("https://9cd6b8b5de12be68eb47:7c1c1189729651b135f1@api.pusherapp.com/apps/79075");
