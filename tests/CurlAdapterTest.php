229c8ccd5c5c6c631515fa9c1db8f3a19c9aa2b8
James Tancock <tanuck@gmail.com>
2015-08-11 20:24:22 +0100

    Testing of the HTTP adapters

---
+++ tests/CurlAdapterTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Pusher\Tests;
+
+use Pusher\CurlAdapter;
+
+class CurlAdapterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsSupported()
+    {
+        $this->assertTrue(CurlAdapter::isSupported());
+    }
+
+    public function testRequest()
+    {
+        $Adapter = new CurlAdapter(array(CURLOPT_TIMEOUT => 30));
+
+        $result = $Adapter->request(
+            'GET',
+            'https://pusher.com',
+            array(
+                'User-Agent: pusher-php-test',
+                'Accept: text/html',
+                'Connection: keep-alive',
+            ),
+            null,
+            10,
+            null
+        );
+        $this->assertEquals(200, $result['status']);
+
+        $result = $Adapter->request(
+            'POST',
+            'https://pusher.com',
+            array(
+                'User-Agent: pusher-php-test',
+                'Accept: text/html',
+                'Connection: keep-alive',
+            ),
+            array('foo' => 'bar'),
+            10,
+            null
+        );
+        $this->assertEquals(404, $result['status']);
+        $this->assertContains('This ain\'t what you were looking for', $result['body']);
+
+        unset($Adapter);
+    }
+}
