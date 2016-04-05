a8e5c32b490f6419febd57c8aad4e915232349c9
zimbatm <zimbatm@zimbatm.com>
2016-04-05 06:37:41 -0400

    Applied fixes from StyleCI

--- test/acceptance/channelQueryTest.php
+++ test/acceptance/channelQueryTest.php
@@ -1,128 +1,125 @@
 <?php
 
-	class PusherChannelQueryTest extends PHPUnit_Framework_TestCase
-	{
+    class PusherChannelQueryTest extends PHPUnit_Framework_TestCase
+    {
+        protected function setUp()
+        {
+            $this->pusher = new Pusher(PUSHERAPP_AUTHKEY, PUSHERAPP_SECRET, PUSHERAPP_APPID, true, PUSHERAPP_HOST);
+            $this->pusher->set_logger(new TestLogger());
+        }
 
-		protected function setUp()
-		{
-			$this->pusher = new Pusher(PUSHERAPP_AUTHKEY, PUSHERAPP_SECRET, PUSHERAPP_APPID, true, PUSHERAPP_HOST);
-			$this->pusher->set_logger( new TestLogger() );
-		}
+        public function testChannelInfo()
+        {
+            $response = $this->pusher->get_channel_info('channel-test');
 
-		public function testChannelInfo()
-		{
-			$response = $this->pusher->get_channel_info('channel-test');
+            //print_r( $response );
 
-			//print_r( $response );
+            $this->assertObjectHasAttribute('occupied', $response, 'class has occupied attribute');
+        }
 
-			$this->assertObjectHasAttribute('occupied', $response, 'class has occupied attribute');
-		}
+        public function testChannelList()
+        {
+            $result = $this->pusher->get_channels();
+            $channels = $result->channels;
 
-		public function testChannelList()
-		{
-			$result = $this->pusher->get_channels();
-			$channels = $result->channels;
+             // print_r( $channels );
 
-			 // print_r( $channels );
+            foreach ($channels as $channel_name => $channel_info) {
+                echo  "channel_name: $channel_name\n";
+                echo  'channel_info: ';
+                print_r($channel_info);
+                echo  "\n\n";
+            }
 
-			foreach( $channels as $channel_name => $channel_info ) {
-        echo( "channel_name: $channel_name\n");
-        echo( 'channel_info: ' );
-        print_r( $channel_info );
-        echo( "\n\n");
-      }
+            $this->assertTrue(is_array($channels), 'channels is an array');
+        }
 
-			$this->assertTrue( is_array($channels), 'channels is an array' );
-		}
-
-		public function testFilterByPrefixNoChannels()
-		{
-			$options = array(
-				'filter_by_prefix' => '__fish'
-			);
-		  $result = $this->pusher->get_channels( $options );
-
-// print_r( $result );
-
-		  $channels = $result->channels;
-
-		  // print_r( $channels );
-
-			$this->assertTrue( is_array($channels), 'channels is an array' );
-			$this->assertEquals( 0, count( $channels ), 'should be an empty array' );
-		}
-
-		public function testFilterByPrefixOneChannel()
-		{
-			$options = array(
-				'filter_by_prefix' => 'test_'
-			);
-		  $result = $this->pusher->get_channels( $options );
+        public function testFilterByPrefixNoChannels()
+        {
+            $options = array(
+                'filter_by_prefix' => '__fish',
+            );
+            $result = $this->pusher->get_channels($options);
 
 // print_r( $result );
 
-		  $channels = $result->channels;
+          $channels = $result->channels;
 
-		  // print_r( $channels );
+          // print_r( $channels );
 
-			$this->assertEquals( 1, count( $channels ), 'channels have a single test-channel present. For this test to pass you must have your API Access setting open for the application you are testing against' );
-		}
+            $this->assertTrue(is_array($channels), 'channels is an array');
+            $this->assertEquals(0, count($channels), 'should be an empty array');
+        }
 
+        public function testFilterByPrefixOneChannel()
+        {
+            $options = array(
+                'filter_by_prefix' => 'test_',
+            );
+            $result = $this->pusher->get_channels($options);
 
-		public function test_providing_info_parameter_with_prefix_query_fails_for_public_channel()
-		{
-			$options = array(
-				'filter_by_prefix' => 'test_',
-				'info' => 'user_count'
-			);
-		  $result = $this->pusher->get_channels( $options );
+// print_r( $result );
 
-			$this->assertFalse( $result, 'query should fail' );
-		}
+          $channels = $result->channels;
 
-		public function test_channel_list_using_generic_get() {
-			$response = $this->pusher->get( '/channels' );
+          // print_r( $channels );
 
-			$this->assertEquals( $response[ 'status' ], 200 );
+            $this->assertEquals(1, count($channels), 'channels have a single test-channel present. For this test to pass you must have your API Access setting open for the application you are testing against');
+        }
 
-			$result = $response[ 'result' ];
+        public function test_providing_info_parameter_with_prefix_query_fails_for_public_channel()
+        {
+            $options = array(
+                'filter_by_prefix' => 'test_',
+                'info' => 'user_count',
+            );
+            $result = $this->pusher->get_channels($options);
 
-			$channels = $result[ 'channels' ];
+            $this->assertFalse($result, 'query should fail');
+        }
 
-			$this->assertEquals( 1, count( $channels ), 'channels have a single test-channel present. For this test to pass you must have your API Access setting open for the application you are testing against' );
+        public function test_channel_list_using_generic_get()
+        {
+            $response = $this->pusher->get('/channels');
 
-			$test_channel = $channels[ 'test_channel' ];
+            $this->assertEquals($response['status'], 200);
 
-			$this->assertEquals( 0, count( $test_channel ) );
-		}
+            $result = $response['result'];
 
-		public function test_channel_list_using_generic_get_and_prefix_param() {
-			$response = $this->pusher->get( '/channels', array( 'filter_by_prefix' => 'test_' ) );
+            $channels = $result['channels'];
 
-			$this->assertEquals( $response[ 'status' ], 200 );
+            $this->assertEquals(1, count($channels), 'channels have a single test-channel present. For this test to pass you must have your API Access setting open for the application you are testing against');
 
-			$result = $response[ 'result' ];
+            $test_channel = $channels['test_channel'];
 
-			$channels = $result[ 'channels' ];
+            $this->assertEquals(0, count($test_channel));
+        }
 
-			$this->assertEquals( 1, count( $channels ), 'channels have a single test-channel present. For this test to pass you must have your API Access setting open for the application you are testing against' );
+        public function test_channel_list_using_generic_get_and_prefix_param()
+        {
+            $response = $this->pusher->get('/channels', array('filter_by_prefix' => 'test_'));
 
-			$test_channel = $channels[ 'test_channel' ];
+            $this->assertEquals($response['status'], 200);
 
-			$this->assertEquals( 0, count( $test_channel ) );
-		}
+            $result = $response['result'];
 
-		public function test_single_channel_info_using_generic_get()
-		{
-			$response = $this->pusher->get( '/channels/channel-test' );
+            $channels = $result['channels'];
 
-			$this->assertEquals( $response[ 'status' ], 200 );
+            $this->assertEquals(1, count($channels), 'channels have a single test-channel present. For this test to pass you must have your API Access setting open for the application you are testing against');
 
-			$result = $response[ 'result' ];
+            $test_channel = $channels['test_channel'];
 
-			$this->assertArrayHasKey('occupied', $result, 'class has occupied attribute');
-		}
+            $this->assertEquals(0, count($test_channel));
+        }
 
-	}
+        public function test_single_channel_info_using_generic_get()
+        {
+            $response = $this->pusher->get('/channels/channel-test');
 
-?>
+            $this->assertEquals($response['status'], 200);
+
+            $result = $response['result'];
+
+            $this->assertArrayHasKey('occupied', $result, 'class has occupied attribute');
+        }
+    }
--- test/unit/pusherConstructorTest.php
+++ test/unit/pusherConstructorTest.php
@@ -1,130 +1,141 @@
 <?php
 
-	class PusherConstructorTest extends PHPUnit_Framework_TestCase
-	{
+    class pusherConstructorTest extends PHPUnit_Framework_TestCase
+    {
+        protected function setUp()
+        {
+        }
 
-		protected function setUp()
-		{
-		}
+        public function testDebugCanBeSetViaLegacyParameter()
+        {
+            $pusher = new Pusher('app_key', 'app_secret', 'app_id', true);
 
-		public function testDebugCanBeSetViaLegacyParameter() {
-			$pusher = new Pusher( 'app_key', 'app_secret', 'app_id', true );
+            $settings = $pusher->getSettings();
+            $this->assertEquals(true, $settings['debug']);
+        }
 
-			$settings = $pusher->getSettings();
-			$this->assertEquals( true, $settings[ 'debug' ] );
-		}
+        public function testHostAndSchemeCanBeSetViaLegacyParameter()
+        {
+            $scheme = 'http';
+            $host = 'test.com';
+            $legacy_host = "$scheme://$host";
+            $pusher = new Pusher('app_key', 'app_secret', 'app_id', false, $legacy_host);
 
-		public function testHostAndSchemeCanBeSetViaLegacyParameter() {
-			$scheme = 'http';
-			$host = 'test.com';
-			$legacy_host = "$scheme://$host";
-			$pusher = new Pusher( 'app_key', 'app_secret', 'app_id', false, $legacy_host );
+            $settings = $pusher->getSettings();
+            $this->assertEquals($scheme, $settings['scheme']);
+            $this->assertEquals($host, $settings['host']);
+        }
 
-			$settings = $pusher->getSettings();
-			$this->assertEquals( $scheme, $settings[ 'scheme' ] );
-			$this->assertEquals( $host, $settings[ 'host' ] );
-		}
+        public function testLegacyHostParamWithNoSchemeCanBeUsedResultsInHostBeingUsedWithDefaultScheme()
+        {
+            $host = 'test.com';
+            $pusher = new Pusher('app_key', 'app_secret', 'app_id', false, $host);
 
-		public function testLegacyHostParamWithNoSchemeCanBeUsedResultsInHostBeingUsedWithDefaultScheme() {
-			$host = 'test.com';
-			$pusher = new Pusher( 'app_key', 'app_secret', 'app_id', false, $host );
+            $settings = $pusher->getSettings();
+            $this->assertEquals($host, $settings['host']);
+        }
 
-			$settings = $pusher->getSettings();
-			$this->assertEquals( $host, $settings[ 'host' ] );
-		}
+        public function testSchemeIsSetViaLegacyParameter()
+        {
+            $host = 'https://test.com';
+            $port = 90;
+            $pusher = new Pusher('app_key', 'app_secret', 'app_id', false, $host, $port);
 
-		public function testSchemeIsSetViaLegacyParameter() {
-			$host = 'https://test.com';
-			$port = 90;
-			$pusher = new Pusher( 'app_key', 'app_secret', 'app_id', false, $host, $port );
+            $settings = $pusher->getSettings();
+            $this->assertEquals('https', $settings['scheme']);
+        }
 
-			$settings = $pusher->getSettings();
-			$this->assertEquals( 'https', $settings[ 'scheme' ] );
-		}
+        public function testPortCanBeSetViaLegacyParameter()
+        {
+            $host = 'https://test.com';
+            $port = 90;
+            $pusher = new Pusher('app_key', 'app_secret', 'app_id', false, $host, $port);
 
-		public function testPortCanBeSetViaLegacyParameter() {
-			$host = 'https://test.com';
-			$port = 90;
-			$pusher = new Pusher( 'app_key', 'app_secret', 'app_id', false, $host, $port );
+            $settings = $pusher->getSettings();
+            $this->assertEquals($port, $settings['port']);
+        }
 
-			$settings = $pusher->getSettings();
-			$this->assertEquals( $port, $settings[ 'port' ] );
-		}
+        public function testTimeoutCanBeSetViaLegacyParameter()
+        {
+            $host = 'http://test.com';
+            $port = 90;
+            $timeout = 90;
+            $pusher = new Pusher('app_key', 'app_secret', 'app_id', false, $host, $port, $timeout);
 
-		public function testTimeoutCanBeSetViaLegacyParameter() {
-			$host = 'http://test.com';
-			$port = 90;
-			$timeout = 90;
-			$pusher = new Pusher( 'app_key', 'app_secret', 'app_id', false, $host, $port, $timeout );
+            $settings = $pusher->getSettings();
+            $this->assertEquals($timeout, $settings['timeout']);
+        }
 
-			$settings = $pusher->getSettings();
-			$this->assertEquals( $timeout, $settings[ 'timeout' ] );
-		}
+        public function testEncryptedOptionWillSetHostAndPort()
+        {
+            $options = array('encrypted' => true);
+            $pusher = new Pusher('app_key', 'app_secret', 'app_id', $options);
 
-		public function testEncryptedOptionWillSetHostAndPort() {
-			$options = array( 'encrypted' => true );
-			$pusher = new Pusher( 'app_key', 'app_secret', 'app_id', $options );
+            $settings = $pusher->getSettings();
+            $this->assertEquals('https', $settings['scheme'], 'https');
+            $this->assertEquals('api.pusherapp.com', $settings['host']);
+            $this->assertEquals('443', $settings['port']);
+        }
 
-			$settings = $pusher->getSettings();
-			$this->assertEquals( 'https', $settings[ 'scheme' ], 'https' );
-			$this->assertEquals( 'api.pusherapp.com', $settings[ 'host' ] );
-			$this->assertEquals( '443', $settings[ 'port' ] );
-		}
+        public function testEncryptedOptionWillBeOverwrittenByHostAndPortOptionsSetHostAndPort()
+        {
+            $options = array(
+                'encrytped' => true,
+                'host' => 'test.com',
+                'port' => '3000',
+            );
+            $pusher = new Pusher('app_key', 'app_secret', 'app_id', $options);
 
-		public function testEncryptedOptionWillBeOverwrittenByHostAndPortOptionsSetHostAndPort() {
-			$options = array(
-				'encrytped' => true,
-				'host' => 'test.com',
-				'port' => '3000'
-			);
-			$pusher = new Pusher( 'app_key', 'app_secret', 'app_id', $options );
+            $settings = $pusher->getSettings();
+            $this->assertEquals('http', $settings['scheme']);
+            $this->assertEquals($options['host'], $settings['host']);
+            $this->assertEquals($options['port'], $settings['port']);
+        }
 
-			$settings = $pusher->getSettings();
-			$this->assertEquals( 'http', $settings[ 'scheme' ] );
-			$this->assertEquals( $options[ 'host' ], $settings[ 'host' ] );
-			$this->assertEquals( $options[ 'port' ], $settings[ 'port' ] );
-		}
+        public function testSchemeIsStrippedAndIgnoredFromHostInOptions()
+        {
+            $options = array(
+                'host' => 'https://test.com',
+            );
+            $pusher = new Pusher('app_key', 'app_secret', 'app_id', $options);
 
-		public function testSchemeIsStrippedAndIgnoredFromHostInOptions() {
-			$options = array(
-				'host' => 'https://test.com'
-			);
-			$pusher = new Pusher( 'app_key', 'app_secret', 'app_id', $options );
+            $settings = $pusher->getSettings();
+            $this->assertEquals('http', $settings['scheme']);
+            $this->assertEquals('test.com', $settings['host']);
+        }
 
-			$settings = $pusher->getSettings();
-			$this->assertEquals( 'http', $settings[ 'scheme' ] );
-			$this->assertEquals( 'test.com', $settings[ 'host' ] );
-		}
+        public function testClusterSetsANewHost()
+        {
+            $options = array(
+                'cluster' => 'eu',
+            );
+            $pusher = new Pusher('app_key', 'app_secret', 'app_id', $options);
 
-		public function testClusterSetsANewHost() {
-			$options = array(
-				'cluster' => 'eu'
-			);
-			$pusher = new Pusher( 'app_key', 'app_secret', 'app_id', $options );
+            $settings = $pusher->getSettings();
+            $this->assertEquals('api-eu.pusher.com', $settings['host']);
+        }
 
-			$settings = $pusher->getSettings();
-			$this->assertEquals( 'api-eu.pusher.com', $settings[ 'host' ] );
-		}
+        public function testClusterOptionIsOverriddenByHostIfItExists()
+        {
+            $options = array(
+                'cluster' => 'eu',
+                'host' => 'api.staging.pusher.com',
+            );
+            $pusher = new Pusher('app_key', 'app_secret', 'app_id', $options);
 
-		public function testClusterOptionIsOverriddenByHostIfItExists() {
-			$options = array(
-				'cluster' => 'eu',
-				'host' => 'api.staging.pusher.com'
-			);
-			$pusher = new Pusher( 'app_key', 'app_secret', 'app_id', $options );
+            $settings = $pusher->getSettings();
+            $this->assertEquals('api.staging.pusher.com', $settings['host']);
+        }
 
-			$settings = $pusher->getSettings();
-			$this->assertEquals( 'api.staging.pusher.com', $settings[ 'host' ] );
-		}
+        public function testClusterOptionIsOverriddenByLegacyHostParameter()
+        {
+            $options = array(
+                'cluster' => 'eu',
+            );
+            $host = 'api.staging.pusher.com';
+            $pusher = new Pusher('app_key', 'app_secret', 'app_id', $options, $host);
 
-		public function testClusterOptionIsOverriddenByLegacyHostParameter() {
-			$options = array(
-				'cluster' => 'eu',
-			);
-			$host = 'api.staging.pusher.com';
-			$pusher = new Pusher( 'app_key', 'app_secret', 'app_id', $options, $host );
-
-			$settings = $pusher->getSettings();
-			$this->assertEquals( 'api.staging.pusher.com', $settings[ 'host' ] );
-		}
-	}
+            $settings = $pusher->getSettings();
+            $this->assertEquals('api.staging.pusher.com', $settings['host']);
+        }
+    }
