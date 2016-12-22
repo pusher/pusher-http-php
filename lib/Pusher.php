7101edc482d1b1de027abadd7a80a0d00222ef08
Luis Fonseca <luis@pusher.com>
2016-12-22 10:46:28 +0000

    Bump version to 2.6.3

--- lib/Pusher.php
+++ lib/Pusher.php
@@ -58,7 +58,7 @@ class PusherInstance
 
 class Pusher
 {
-    public static $VERSION = '2.5.0-rc3';
+    public static $VERSION = '2.6.3';
 
     private $settings = array(
         'scheme'       => 'http',
