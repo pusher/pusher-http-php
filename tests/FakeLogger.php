5c2c7d916a3acff6448d937769e62da5939a9192
benjaco <benjaco1996@gmail.com>
2018-01-09 04:24:08 +0100

    Improved php docs Improved logger validation

---
+++ tests/FakeLogger.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Benjamin
+ * Date: 09-01-2018
+ * Time: 04:02
+ */
+
+class FakeLogger {
+}
---
+++ tests/acceptance/loggerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+class PusherLoggerTest extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->pusher = new Pusher\Pusher("", "", "");
+    }
+
+
+    public function tesSetRealLogger()
+    {
+        $this->pusher->set_logger(new TestLogger());
+    }
+
+    /**
+     * @expectedException \Pusher\PusherException
+     */
+    public function testSetFakeLogger()
+    {
+        $this->pusher->set_logger(new FakeLogger());
+
+    }
+
+}
