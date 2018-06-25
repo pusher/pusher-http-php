d88cb6e2650522dc9505ca23f5e8c29956f694db
kn100 <backupa76@gmail.com>
2018-06-25 15:09:08 +0100

    refactor wrt wills review

--- src/Webhook.php
+++ src/Webhook.php
@@ -4,6 +4,19 @@ namespace Pusher;
 
 class Webhook
 {
-    public $time_ms;
-    public $events = array();
+    private $time_ms;
+    private $events = array();
+    
+    public function __construct($time_ms, $events) {
+        $this->time_ms = $time;
+        $this->events = $events;
+    }
+
+    public function get_webhooks() {
+        return $this->events;
+    }
+
+    public function get_time_ms() {
+        return $this->time_ms;
+    }
 }
