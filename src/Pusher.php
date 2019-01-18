318e76514579cd77c9cf8dbe270c0c831ff9bd0b
Damiano Donati <damiano.donati@gmail.com>
2019-01-18 12:07:01 +0000

    v3.3.1

--- src/Pusher.php
+++ src/Pusher.php
@@ -14,7 +14,7 @@ class Pusher implements LoggerAwareInterface
     /**
      * @var string Version
      */
-    public static $VERSION = '3.3.0';
+    public static $VERSION = '3.3.1';
 
     /**
      * @var null|PusherCrypto
