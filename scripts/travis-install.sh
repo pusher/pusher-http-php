ad8b3d1e27c08e35b112605ab3fc76542ad9e2d5
Will Sewell <me@willsewell.com>
2019-06-17 14:31:13 +0100

    Drop support for PHP 5.x and 7.0

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -8,9 +8,5 @@ composer install --no-interaction --prefer-source
 if [ $INSTALL_LIBSODIUM = true ]; then
   sudo add-apt-repository ppa:ondrej/php -y
   sudo apt-get update && sudo apt-get install libsodium-dev -y
-  if [ $TRAVIS_PHP_VERSION == '5.4' ] || [ $TRAVIS_PHP_VERSION == '5.5' ] || [ $TRAVIS_PHP_VERSION == '5.6' ]; then
-    pecl install libsodium-1.0.7
-  else
-    pecl install libsodium-2.0.11
-  fi
+  pecl install libsodium-2.0.11
 fi
